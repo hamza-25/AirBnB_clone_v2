14bf61e56213198fffc20ec2cc763be9d5b882e1
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-11-26 04:44:42 +0200

    up1

--- models/review.py
+++ models/review.py
@@ -8,9 +8,3 @@ class Review(BaseModel):
     place_id = ""
     user_id = ""
     text = ""
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-        places = relationship("Place", backref="users", cascade="all, delete")
-        user = relationship("User", backref='reviews')
