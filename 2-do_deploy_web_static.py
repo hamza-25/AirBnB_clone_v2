0a686e2c1e99b8967b7165155ed1251b7556b740
hamza-25 <me677499@gmail.com>
2023-12-07 14:40:16 +0100

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,19 +6,26 @@ from fabric.api import put
 import os
 
 
+env.hosts = ['100.24.238.196', '23.23.74.47']
+env.user = 'ubuntu'
+
+
 def do_deploy(archive_path):
     """archive files web_static"""
-    env.user = 'ubuntu'
-    env.hosts = ['100.24.238.196', '23.23.74.47']
     if not os.path.exists(archive_path):
         return False
     try:
         file_name = path.split('/')[-1]
-        put(archive_path, f'/tmp/{file_name}')
-        run(f'tar -xzvf /tmp/{file_name} -C /data/web_static/releases/')
+        w_file_name = file_name.split('.')[0]
+        put(archive_path, '/tmp/')
+        run(f'mkdir -p /data/web_static/releases/{w_file_name}/')
+        run(f'tar -xzvf /tmp/{file_name} \
+                -C /data/web_static/releases/{w_file_name}/')
         run(f'rm /tmp/{file_name}')
         run('rm -f /data/web_static/current')
-        run('ln -s /data/web_static/releases/ /data/web_static/current')
+        run(f'ln -s /data/web_static/releases/{w_file_name}/ \
+                /data/web_static/current')
+        print("New version deployed!")
         return True
     except Exception:
         return False
