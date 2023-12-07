8a5f9f63cf9bdf6ec64b080e0879af3b43cb79ae
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-12-07 17:57:46 +0100

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """archive module"""
 from fabric.api import env
+from fabric.api import local
 from fabric.api import run
 from fabric.api import put
+from datetime import datetime
 import os
 
 env.hosts = ['100.24.238.196', '23.23.74.47']
@@ -31,21 +33,20 @@ def do_deploy(archive_path):
         r_path = f'/data/web_static/releases/{w_file_name}/'
         tmp_path = f'/tmp/{file_name}'
         put(archive_path, '/tmp/')
-        run(f'mkdir -p {r_path}')
-        run(f'tar -xzf {tmp_path} -C {r_path}')
-        run(f'rm {tmp_path}')
-        run(f"mv {r_path}web_static/* {r_path}")
-        run(f"rm -rf {r_path}web_static")
-        run('rm -rf /data/web_static/current')
-        run(f'ln -s {r_path} /data/web_static/current')
-        print("New version deployed!")
+        run(f'sudo mkdir -p {r_path}')
+        run(f'sudo tar -xzf {tmp_path} -C {r_path}')
+        run(f'sudo rm {tmp_path}')
+        run(f"sudo mv {r_path}web_static/* {r_path}")
+        run(f"sudo rm -rf {r_path}web_static")
+        run('sudo rm -rf /data/web_static/current')
+        run(f'sudo ln -s {r_path} /data/web_static/current')
         return True
-    except Exception as e:
+    except BaseException as e:
         return False
 
     def deploy():
         """full deploy"""
-        path_arch = do_pack()
+        archive_path = do_pack()
         if path_arch is None:
             False
-        return do_deploy(path_arch)
+        return do_deploy(archive_path)
