c75beba00bc3db30c7fe64dbac2f2a0056e142a5
hamza-25 <me677499@gmail.com>
2023-12-07 15:49:08 +0100

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,7 @@ from fabric.api import env
 from fabric.api import run
 from fabric.api import put
 import os
-
+import shlex
 
 env.hosts = ['100.24.238.196', '23.23.74.47']
 env.user = 'ubuntu'
@@ -15,8 +15,13 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
     try:
-        file_name = archive_path.split('/')[-1]
-        w_file_name = file_name.split('.')[0]
+        file_name = archive_path.replace('/', ' ')
+        file_name = shlex.split(file_name)
+        file_name = file_name[-1]
+        # w_file_name = file_name.split('.')[0]
+        w_file_name = file_name.replace('.', ' ')
+        w_file_name = shlex.split(w_file_name)
+        w_file_name = w_file_name[0]
         r_path = f'/data/web_static/releases/{w_file_name}/'
         tmp_path = f'/tmp/{file_name}'
         put(archive_path, '/tmp/')
