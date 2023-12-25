8cc7b60669f6b743eabd92e83e383b87d5a7567f
hamza-25 <me677499@gmail.com>
2023-12-25 23:56:03 +0100

    update

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -3,6 +3,7 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.place import Place
 from models.amenity import Amenity
 
 app = Flask(__name__)
@@ -14,11 +15,13 @@ def handle_teardow(self):
     storage.close()
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
-def hbnb_filters():
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
     states = storage.all(State).values()
     amenities = storage.all(Amenity).values()
-    return render_template('10-hbnb_filters.html', states=states, amenities=amenities)
+    places = storage.all(Place).values()
+    return render_template('100-hbnb.html', states=states,
+                           amenities=amenities, places=places)
 
 
 if __name__ == '__main__':
